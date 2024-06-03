46fc23461958a1db360794044903a37ede4f2cd9
khalil-ali1 <khalil.ali.uk1@gmail.com>
2024-06-03 16:10:01 +0100

    a

--- src/components/1-header/Header.jsx
+++ src/components/1-header/Header.jsx
@@ -3,7 +3,27 @@ import './header.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+    <nav>
+      <ul>
+      <li>
+        <a href="">About</a>
+      </li>
+      <li>
+        <a href="">Articles</a>
+      </li>
+      <li>
+        <a href="">Projects</a>
+      </li>
+      <li>
+        <a href="">Speaking</a>
+      </li>
+      <li>
+        <a href="">Contact</a>
+      </li>
+      </ul>
+      </nav>
+    </header>
   )
 }
 
