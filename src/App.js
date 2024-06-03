d6ea5ff9f0417f43d34a91a49275a25ebe923030
khalil-ali1 <khalil.ali.uk1@gmail.com>
2024-06-03 11:41:32 +0100

    a

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
-import Header from 'components/1-header/Header'
+import Header from './components/1-header/Header'
+import Hero from './components/2-hero/Hero'
+import Main from './components/3-main/Main'
+import Contact from './components/4-contact/Contact'
+import Footer from './components/5-footer/Footer'
+
 
 
 function App() {
