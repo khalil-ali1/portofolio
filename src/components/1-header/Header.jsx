8b9ddac7ac40d83d648ec122a3e9c0184a3fc200
khalil-ali1 <khalil.ali.uk1@gmail.com>
2024-06-04 03:31:52 +0100

    a

--- src/components/1-header/Header.jsx
+++ src/components/1-header/Header.jsx
@@ -1,28 +1,28 @@
-import React from 'react'
 import './header.css'
 
 const Header = () => {
   return (
-    <header>
-    <nav>
-      <ul>
-      <li>
-        <a href="">About</a>
-      </li>
-      <li>
-        <a href="">Articles</a>
-      </li>
-      <li>
-        <a href="">Projects</a>
-      </li>
-      <li>
-        <a href="">Speaking</a>
-      </li>
-      <li>
-        <a href="">Contact</a>
-      </li>
+    <header className='flex'>
+      <nav>
+      <ul className='flex'>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Articles</a>
+          </li>
+          <li>
+            <a href="#">Projects</a>
+          </li>
+          <li>
+            <a href="#">Speaking</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
       </ul>
       </nav>
+      <button>light</button>
     </header>
   )
 }
